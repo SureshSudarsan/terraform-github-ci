df009f52f629a04111548ed7164acee960ca10be
SureshSudarsan <sureshsudarsan1957@gmail.com>
2026-03-16 07:48:58 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,47 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_security_group" "web_sg" {
+  name = "terraform-web-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install nginx -y
+              echo "Hello from Terraform GitHub CI/CD Project" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Terraform-GitHub-WebServer"
+  }
+}
